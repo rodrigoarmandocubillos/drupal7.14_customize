88bab573054ac7111f2a4e3ba0baf067cf427203
rodrigoarmandocubillos <rodri_cubillos@etb.net.co>
2012-06-27 17:15:05 -0500

    test update

--- profiles/axiacore/modules/htmlmail/htmlmail--simplenews.tpl.php
+++ profiles/axiacore/modules/htmlmail/htmlmail--simplenews.tpl.php
@@ -61,7 +61,7 @@
 ?>
 <?php if ($key == 'node' || $key == 'test'): ?>
 <div class="htmlmail-simplenews-link">
-  <a href="<?php echo url('node/' . $params['context']['node']->nid, array('absolute' => TRUE)); ?>">
+  <a href="<?php echo url('node/' . $params['simplenews_source']->getNode()->nid, array('absolute' => TRUE)); ?>">
     Click here to view this message on the web.
   </a>
 </div>
